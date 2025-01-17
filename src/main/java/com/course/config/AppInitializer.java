18f807db2407321bf61dd27e74aa629f7a67e38c
Nguyễn Đức Mạnh <dmanhpt@gmail.com>
2025-01-17 09:19:18 +0700

    Feature bug#1-fix-env

--- src/main/java/com/course/config/AppInitializer.java
+++ src/main/java/com/course/config/AppInitializer.java
@@ -3,6 +3,7 @@ package com.course.config;
 import com.course.common.utils.HibernateUtils;
 import com.course.config.filter.AccessTokenFilter;
 import com.course.config.filter.RefreshTokenFilter;
+import com.course.config.properties.SecurityProperties;
 import jakarta.servlet.FilterRegistration;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
@@ -16,6 +17,15 @@ public class AppInitializer implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
+
+        SecurityProperties securityProperties = SecurityProperties.getInstance();
+
+        // Log properties for debugging (remove in production)
+        System.out.println("Loaded Security Properties:");
+        System.out.println("JWT Secret: " + securityProperties.getJwtSecret());
+        System.out.println("Access Token Expiration: " + securityProperties.getJwtAccessExpiration());
+        System.out.println("Refresh Token Expiration: " + securityProperties.getJwtRefreshExpiration());
+        System.out.println("Remember Me Expiration: " + securityProperties.getRememberMeExpiration());
 
         // Đăng ký AccessTokenFilter cho các URL bắt đầu bằng /api
         FilterRegistration.Dynamic accessTokenFilter = servletContext.addFilter("accessTokenFilter", new AccessTokenFilter());
--- src/main/java/com/course/config/properties/SecurityProperties.java
+++ src/main/java/com/course/config/properties/SecurityProperties.java
@@ -1,12 +1,45 @@
 package com.course.config.properties;
 
 
-public interface SecurityProperties {
-    String jwtSecret = "MWM0NzE4MDk2MGQwMWFkNjZiNDQ5ZTJkMTJjYTE2N2M1YTFhY2E3M2UzMzBlMGMzZjU3OGVhMGQwMmQyZGM5OTgyODJlOWIwZGVhMzJkOTUzNTdlMjM4ZDIxMTk0YjgzNmEzNDNlMTBjZTMwMGMyNjgzYTc2ZTlmZjE5MzZkZmM";
-    //# Access token is valid 15 minute 900
-    long jwtAccessExpiration = 900;
-    //# Refresh token is valid 3 days
-    long jwtRefreshExpiration = 259200;
-    //# Remember me is valid 30 days
-    long rememberMeExpiration = 2592000;
+import lombok.Getter;
+import lombok.Setter;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.util.Map;
+
+@Getter
+@Setter
+public class SecurityProperties {
+    private static final SecurityProperties INSTANCE = new SecurityProperties();
+    private String jwtSecret;
+    private long jwtAccessExpiration;
+    private long jwtRefreshExpiration;
+    private long rememberMeExpiration;
+
+    private SecurityProperties() {
+        loadProperties();
+    }
+
+    public static SecurityProperties getInstance() {
+        return INSTANCE;
+    }
+
+    private void loadProperties() {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml")) {
+            if (inputStream == null) {
+                throw new IllegalStateException("Configuration file 'config.yml' not found in classpath.");
+            }
+            Map<String, Object> config = new Yaml().load(inputStream);
+
+            Map<String, Object> jwtConfig = (Map<String, Object>) ((Map<String, Object>) config.get("security")).get("jwt");
+            this.jwtSecret = (String) jwtConfig.get("secret");
+            this.jwtAccessExpiration = ((Number) jwtConfig.get("accessExpiration")).longValue();
+            this.jwtRefreshExpiration = ((Number) jwtConfig.get("refreshExpiration")).longValue();
+            this.rememberMeExpiration = ((Number) jwtConfig.get("rememberMeExpiration")).longValue();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load configuration", e);
+        }
+    }
+
 }
